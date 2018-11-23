4cb62b43a850c7e80d768e192e455e29d9707130
matt.pis@yahoo.com <matt@matts-macbook.local>
2018-11-23 14:07:05 -0600

    Initial Commit

---
+++ PadRoom_MacTests/PadRoom_MacTests.swift
@@ -0,0 +1,34 @@
+//
+//  PadRoom_MacTests.swift
+//  PadRoom_MacTests
+//
+//  Created by Matt on 11/23/18.
+//  Copyright © 2018 Matt. All rights reserved.
+//
+
+import XCTest
+@testable import PadRoom_Mac
+
+class PadRoom_MacTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
